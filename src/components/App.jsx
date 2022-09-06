efbaa166abcd6fffd037351f9d033e86eadb4844
shuriuss <104681622+shuriuss@users.noreply.github.com>
2022-09-06 20:21:07 +0300

    add css task#3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -35,7 +35,6 @@ export const App = () => {
 
 <Statistics title="Upload stats" stats={data} />;
 <Statistics stats={data} />;
-
 <FriendList friends={friends} />;
 <TransactionHistory items={transactions} />;
 </div>
--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,12 +1,11 @@
 import PropTypes from 'prop-types';
-// import friends from '../../data/friends.json'
 import s from './FriendListItem.module.css'
 
 
 function FriendListItem({avatar, name, isOnline=false}) {
     return (
 <>
-  <span className={s.status}></span>
+  <span className={isOnline ? s.statusOn : s.statusOff}></span>
   <img className={s.avatar} src={avatar} alt="User avatar" width="48" />
   <p className={s.name}>{name}</p>
 </>
