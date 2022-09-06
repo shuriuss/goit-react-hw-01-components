a807a0abaf7990a8ae448f14e7ea0a7eaf0ef0a6
shuriuss <104681622+shuriuss@users.noreply.github.com>
2022-09-06 18:30:39 +0300

    add css task#1

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -6,22 +6,22 @@ function Profile({ username, tag, location, avatar, stats }) {
   return (
     <div className={s.profile}>
       <div className={s.description}>
-        <img src={avatar} alt="User avatar" class="avatar" />
-        <p clasNames={s.name}>{username}</p>
+        <img src={avatar} alt="User avatar" className={s.avatar} />
+        <p className={s.name}>{username}</p>
         <p className={s.tag}>@{tag}</p>
         <p className={s.location}>{location}</p>
       </div>
 
       <ul className={s.stats}>
-        <li>
+        <li className={s.statsItem}>
           <span className={s.label}>Followers</span>
-          <span clasNames={s.quantity}>{stats.followers}</span>
+          <span classNames={s.quantity}>{stats.followers}</span>
         </li>
-        <li>
+        <li className={s.statsItem}>
           <span className={s.label}>Views</span>
           <span className={s.quantity}>{stats.views}</span>
         </li>
-        <li>
+        <li className={s.statsItem}>
           <span className={s.label}>Likes</span>
           <span className={s.quantity}>{stats.likes}</span>
         </li>
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,6 +1,5 @@
 import PropTypes from 'prop-types';
-// import data from '../../data/data.json';
-import s from './Statistics.module.css'
+import s from './Statistics.module.css';
 
 function Statistics({ title, stats }) {
   return (
@@ -14,18 +13,18 @@ function Statistics({ title, stats }) {
             <span className={s.percentage}>{item.percentage}</span>
           </li>
         ))}
-        </ul>
+      </ul>
     </section>
   );
 }
 
 Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.shape({
     id: PropTypes.string.isRequired,
-    title: PropTypes.string,
-    stats: PropTypes.shape({
-        label: PropTypes.string.isRequired,
-        percentage: PropTypes.number.isRequired,
-    })
-}
+    label: PropTypes.string.isRequired,
+    percentage: PropTypes.number.isRequired,
+  }),
+};
 
 export default Statistics;
